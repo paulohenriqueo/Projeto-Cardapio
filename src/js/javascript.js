ee2e8246e19900e97b3f8250db7f2e99a4b35bcc
paulohenriqueo <paulohenrique300305@gmail.com>
2025-05-21 20:13:59 -0300

    closes #4 :sparkles: feat: Add Quantidades

---
+++ src/js/javascript.js
@@ -0,0 +1,8 @@
+var prods = [
+    {id : 1, name : "Bife com batata", price : 30.00},
+    {id : 2, name : "Coxa de Frango Crocante", price : 25.00},
+    {id : 3, name : "Carne de Panela", price : 22.00},
+    {id : 4, name : "Farofa", price : 10.00},
+    {id : 5, name : "Salada", price : 8.00},
+    {id : 6, name : "Torresmo", price : 12.00},
+]
